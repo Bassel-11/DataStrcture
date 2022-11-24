780be7e4962dc2d6b1884674cb210f897b2be8c5
Albassel Abobakr <100967694+Bassel-11@users.noreply.github.com>
2022-11-24 10:37:57 +0200

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,80 @@
+//⣿⣿⣿⣿⣯⣿⣿⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠈⣿⣿⣿⣿⣿⣿⣆⠄
+//⢻⣿⣿⣿⣾⣿⢿⣢⣞⣿⣿⣿⣿⣷⣶⣿⣯⣟⣿⢿⡇⢃⢻⣿⣿⣿⣿⣿⢿⡄
+//⠄⢿⣿⣯⣏⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣧⣾⢿⣮⣿⣿⣿⣿⣾⣷
+//⠄⣈⣽⢾⣿⣿⣿⣟⣄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣝⣯⢿⣿⣿⣿⣿
+//⣿⠟⣫⢸⣿⢿⣿⣾⣿⢿⣿⣿⢻⣿⣿⣿⢿⣿⣿⣿⢸⣿⣼⣿⣿⣿⣿⣿⣿⣿
+//⡟⢸⣟⢸⣿⠸⣷⣝⢻⠘⣿⣿⢸⢿⣿⣿⠄⣿⣿⣿⡆⢿⣿⣼⣿⣿⣿⣿⢹⣿
+//⡇⣿⡿⣿⣿⢟⠛⠛⠿⡢⢻⣿⣾⣞⣿⡏⠖⢸⣿⢣⣷⡸⣇⣿⣿⣿⢼⡿⣿⣿
+//⣡⢿⡷⣿⣿⣾⣿⣷⣶⣮⣄⣿⣏⣸⣻⣃⠭⠄⠛⠙⠛⠳⠋⣿⣿⣇⠙⣿⢸⣿
+//⠫⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⠻⣿⣾⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣹⢷⣿⡼⠋
+//⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⠄⠄
+//⠄⠄⢻⢹⣿⠸⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣼⣿⣿⣿⣿⡟⠄⠄
+//⠄⠄⠈⢸⣿⠄⠙⢿⣿⣿⣹⣿⣿⣿⣿⣟⡃⣽⣿⣿⡟⠁⣿⣿⢻⣿⣿⢿⠄⠄
+//⠄⠄⠄⠘⣿⡄⠄⠄⠙⢿⣿⣿⣾⣿⣷⣿⣿⣿⠟⠁⠄⠄⣿⣿⣾⣿⡟⣿⠄⠄
+//⠄⠄⠄⠄⢻⡇⠸⣆⠄⠄⠈⠻⣿⡿⠿⠛⠉⠄⠄⠄⠄⢸⣿⣇⣿⣿⢿⣿⠄⠄⠀
+
+// IAM Batman
+//           Vengeance
+//                    Maybe im the problem
+#include <bits/stdc++.h>
+#define ll long long
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define ln cout<<endl;
+using namespace std;
+const int size=5;
+int Stack[size];
+int top=-1;
+void push(int value) {
+    if (top == size - 1) {
+        cout << " stack overflow "<<endl;
+    } else {
+        top++;
+        Stack[top] = value;
+    }
+}
+int pop() {
+    if (top==-1)
+        cout<<" stack underflow "<<endl;
+    else {
+        return Stack[top--];
+    }
+}
+int search(int value){
+    if (top==-1)
+        cout<<" stack underflow "<<endl;
+    else{
+        for(int i=top; i>=-1; i--) {
+            if(value==Stack[i]) {
+                cout << i<<endl;
+                return 0;
+            }
+            if(i==-1)
+                cout<<"value does not exist";
+        }
+    }
+}
+int peek(){
+    if(top==-1)
+        cout<<" stack underflow "<<endl;
+    else
+        return Stack[top];
+}
+void display() {
+    if (top==-1)
+        cout<<" stack underflow "<<endl;
+    else {
+        for(int i=top; i>-1; i--)
+            cout<<Stack[i]<<endl;
+    }
+}
+
+int main() {
+    push(34);
+    push(12);
+    push(11);
+    push(10);
+    push(5);
+    display();
+    search(11);
+    return 0;
+}
